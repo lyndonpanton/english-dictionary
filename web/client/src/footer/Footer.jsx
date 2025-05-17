796f5458e0f5d8d0f791a82876b561acbed8bdb5
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-17 21:56:21 +0100

    Added primary elements

---
+++ web/client/src/footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            {/* Set year dynamically */}
+            <p>&copy; 2025 Lyndon Mykal Panton | All Rights Reserved</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ web/client/src/main/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <main>
+            <p>Here is the main content.</p>
+        </main>
+    );
+}
+
+export default Main;
