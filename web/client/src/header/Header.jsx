2630aa22821cf129f6c9f0dea9b297fda331f1cd
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-18 22:04:29 +0100

    Minor changes

--- web/client/src/header/Header.jsx
+++ web/client/src/header/Header.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function Header() {
+    const [searchValue, setSearchValue] = useState("");
+
+    function searchForWord(e) {
+        if (e.key === "Enter") {
+            console.log(e.target.value.toLowerCase());
+        }
+    }
+
     return (
         <header>
             <h1>English Dictionary</h1>
+            <input type="text" onChange={(e) => setSearchValue(e.target.value)} onKeyUp={searchForWord}/>
         </header>
     );
 }
